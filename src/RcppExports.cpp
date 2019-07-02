2b2773a716de3c4403b8a3f5f919ff92e0f248d0
Christopher Eeles <chris@DESKTOP-03V4D4N.localdomain>
2019-07-02 13:53:51 -0400

    CRAN development branch

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -24,21 +24,6 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// solve_by_cplex_cpp
-NumericVector solve_by_cplex_cpp(SEXP my_obj, SEXP my_cons, SEXP my_rhs, SEXP my_lb, SEXP my_ub);
-RcppExport SEXP _rlobico_solve_by_cplex_cpp(SEXP my_objSEXP, SEXP my_consSEXP, SEXP my_rhsSEXP, SEXP my_lbSEXP, SEXP my_ubSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< SEXP >::type my_obj(my_objSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type my_cons(my_consSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type my_rhs(my_rhsSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type my_lb(my_lbSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type my_ub(my_ubSEXP);
-    rcpp_result_gen = Rcpp::wrap(solve_by_cplex_cpp(my_obj, my_cons, my_rhs, my_lb, my_ub));
-    return rcpp_result_gen;
-END_RCPP
-}
 // DNF_ILP_weak_cpp
 List DNF_ILP_weak_cpp(DataFrame X, NumericVector Y, NumericVector W, double K, double M, double lambda, double sens, double spec, NumericMatrix addcons);
 RcppExport SEXP _rlobico_DNF_ILP_weak_cpp(SEXP XSEXP, SEXP YSEXP, SEXP WSEXP, SEXP KSEXP, SEXP MSEXP, SEXP lambdaSEXP, SEXP sensSEXP, SEXP specSEXP, SEXP addconsSEXP) {
@@ -58,11 +43,26 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// solve_by_cplex_cpp
+NumericVector solve_by_cplex_cpp(SEXP my_obj, SEXP my_cons, SEXP my_rhs, SEXP my_lb, SEXP my_ub);
+RcppExport SEXP _rlobico_solve_by_cplex_cpp(SEXP my_objSEXP, SEXP my_consSEXP, SEXP my_rhsSEXP, SEXP my_lbSEXP, SEXP my_ubSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< SEXP >::type my_obj(my_objSEXP);
+    Rcpp::traits::input_parameter< SEXP >::type my_cons(my_consSEXP);
+    Rcpp::traits::input_parameter< SEXP >::type my_rhs(my_rhsSEXP);
+    Rcpp::traits::input_parameter< SEXP >::type my_lb(my_lbSEXP);
+    Rcpp::traits::input_parameter< SEXP >::type my_ub(my_ubSEXP);
+    rcpp_result_gen = Rcpp::wrap(solve_by_cplex_cpp(my_obj, my_cons, my_rhs, my_lb, my_ub));
+    return rcpp_result_gen;
+END_RCPP
+}
 
 static const R_CallMethodDef CallEntries[] = {
     {"_rlobico_CNF_ILP_weak_cpp", (DL_FUNC) &_rlobico_CNF_ILP_weak_cpp, 9},
-    {"_rlobico_solve_by_cplex_cpp", (DL_FUNC) &_rlobico_solve_by_cplex_cpp, 5},
     {"_rlobico_DNF_ILP_weak_cpp", (DL_FUNC) &_rlobico_DNF_ILP_weak_cpp, 9},
+    {"_rlobico_solve_by_cplex_cpp", (DL_FUNC) &_rlobico_solve_by_cplex_cpp, 5},
     {NULL, NULL, 0}
 };
 
