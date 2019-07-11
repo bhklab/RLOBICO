dd4748ddfed01fa430dbbdf0c22bd5a6b5839f20
ChristopherEeles <christopher.eeles@outlook.com>
2019-07-11 17:48:43 -0400

    [Complete] Code clean-up; all functions working [TO-DO] * Determine if a configure script is needed for submission * Get Bo to add better descriptions to the function documentation

--- vignettes/modelling-example.R
+++ vignettes/modelling-example.R
@@ -18,11 +18,9 @@ rownames(MutationMatrix) <- Samples
 
 ## ----Configuring Parameters----------------------------------------------
 ## Create binary input, output, and weight vector
-
 #binary input
 X <- MutationMatrix
 
-#write.csv(X, file="mutaionMatrix.csv", row.names=F, col.names=F)
 N <- nrow(X)
 P <- ncol(X)
 
@@ -44,17 +42,6 @@ K <- 2
 M <- 1
 
 ## ----Cplex Options-------------------------------------------------------
-## Cplex options
-# param <- rbind(list('timelimit.Cur', 60000, 'MaxTime'), #Maximum time for IP )in seconds)
-#                list('mipltolerances.integrality.Cur', 1e-5, 'Integrality'), #Integrality constraint; default = 1e-5 (see cplex.Param.mip.tolerances.integrality.Help)
-#                list('mip.tolerancs.mipgap.Cur', 1e-4, 'RelGap'), #Optimality gap tolerance; default = 1e-4 (0.01% of optimal solution, set to 0.05, 5% for early termination, approximate solution) (see cplex.Param.mip.tolerances.mipgap.Help)
-#                list('threads.Cur', 8, 'Threads'), #Number of threads to use (default = 0, automatic) (see cplex.Param.threads.Help)
-#                list('parallel.Cur', -1, 'ParallelMode'), #Parallel optimization mode, -1 = opportunistic 0 = automatic 1 = deterministic (see plex.Param.parallel.Help)
-#                list('mip.pool.relgap.Cur', 1e-1, 'Pool_relgap'), #Relative gap for suboptimal solutions in the pool; default 0.1 (10%)
-#                list('mip.pool.intensity.Cur', 1, 'Pool_intensity'), #Pool intensity; default 1 = mild: generate few solutions quickly (see cplex.Param.mip.pool.intensity.Help)
-#                list('mip.pool.replace.Cur', 2, 'Pool_replace'), #Pool replacement strategy; default 2 = replace least diverse solutions (see cplex.Param.mip.pool.replace.Help)
-#                list('mip.pool.capacity.Cur', 11, 'Pool_capacity'), #Pool capacity; default 11 = best + 10 (see cplex.Param.mip.pool.replace.Help)
-#                list('mip.limits.populate.Cur', 11, 'Pool_size'))#Number of solutions generated; default 11 = best + 10 (see cplex.Param.mip.limits.populate.Help)
 param <- rbind(list('tilim', 60000, 'MaxTime'), #Maximum time for IP )in seconds)
                list('mipltolerances.integrality.Cur', 1e-5, 'Integrality'), #Integrality constraint; default = 1e-5 (see cplex.Param.mip.tolerances.integrality.Help)
                list('epgap', 1e-4, 'RelGap'), #Optimality gap tolerance; default = 1e-4 (0.01% of optimal solution, set to 0.05, 5% for early termination, approximate solution) (see cplex.Param.mip.tolerances.mipgap.Help)
@@ -83,7 +70,9 @@ param <- data.frame("V1" = param[[1]], "V2" = param[[2]], "V3" = param[[3]])
 sol <- lobico(X = X,
               Y = W,
               K = K,
-              M = M)
+              M = M,
+              solve = 1,
+              param = param)
 
 ## ----Checking Solution---------------------------------------------------
 ## Check solution
